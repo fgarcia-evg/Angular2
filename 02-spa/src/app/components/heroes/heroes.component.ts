4e744ae2af5dbe116dc9aac7ed9626fc6f53fab3
Fernando Herrera <fernando.herrera85@gmail.com>
2020-11-06 12:18:11 +0100

    06-11-2020b

--- 02-spa/src/app/components/heroes/heroes.component.ts
+++ 02-spa/src/app/components/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from '../../servicios/heroes.service';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
@@ -9,13 +9,18 @@ import { HeroesService, Heroe } from '../../servicios/heroes.service';
 export class HeroesComponent implements OnInit {
 
   heroes:any[]=[];
-  constructor( private _heroesService:HeroesService ) {
-      console.log("constructor");
+  constructor( private _heroesService:HeroesService,
+                private _router:Router) {
+     // console.log("constructor");
   }
 
   ngOnInit() {
     this.heroes = this._heroesService.getHeroes();
     console.log(this.heroes);
+  }
+
+  verHeroe( idx: number ) {
+    this._router.navigate( ['/heroe', idx]) ;
   }
 
 }
